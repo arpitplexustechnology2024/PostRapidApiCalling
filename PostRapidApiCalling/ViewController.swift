9731f51abfcc2e6ddad6884cf8018da1ef66d1ff
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-10 14:59:00 -0700

    Initial Commit

---
+++ PostRapidApiCalling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostRapidApiCalling
+//
+//  Created by Arpit iOS Dev. on 10/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
